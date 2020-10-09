e125b4b60616b5e063660fa2a848c5ccaba48dac
Mason Mackaman <masondeanm@aol.com>
2020-10-09 02:07:29 -0400

    Work with the new Task

--- src/Fs.purs
+++ src/Fs.purs
@@ -1,6 +1,7 @@
 module Fs where
 
 import MasonPrelude
+import Effect.Exception (Error)
 import Task (Task, Promise)
 import Task as Task
 
@@ -8,25 +9,28 @@ foreign import exists :: ∀ x. String -> Task x Boolean
 
 foreign import mkdirPromise :: String -> Promise Unit
 
-mkdir :: String -> Task String Unit
-mkdir = Task.fromPromise mkdirPromise
+mkdir :: String -> Task Error Unit
+mkdir = Task.fromPromise <. const <. mkdirPromise
 
 foreign import readdirPromise :: String -> Promise (Array String)
 
-readdir :: String -> Task String (Array String)
-readdir = Task.fromPromise readdirPromise
+readdir :: String -> Task Error (Array String)
+readdir = Task.fromPromise <. const <. readdirPromise
 
 foreign import unlinkPromise :: String -> Promise Unit
 
-unlink :: String -> Task String Unit
-unlink = Task.fromPromise unlinkPromise
+unlink :: String -> Task Error Unit
+unlink = Task.fromPromise <. const <. unlinkPromise
 
 foreign import writeFilePromise :: String -> String -> Promise Unit
 
-writeFile :: String -> String -> Task String Unit
-writeFile path = Task.fromPromise (writeFilePromise path)
+writeFile :: String -> String -> Task Error Unit
+writeFile = Task.fromPromise <. const <.. writeFilePromise
 
 foreign import readFilePromise :: String -> Promise String
 
-readFile :: String -> Task String String
-readFile = Task.fromPromise readFilePromise
+readFile :: String -> Task Error String
+readFile = Task.fromPromise <. const <. readFilePromise
+
+main :: Effect Unit
+main = Task.run $ writeFile "task.txt" "it worked!"
