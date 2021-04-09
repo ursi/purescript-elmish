47deb688c8ec62e15d7dc1264e39998633761803
Mason Mackaman <masondeanm@aol.com>
2021-04-08 22:20:50 -0400

    Update package.nix

--- package.nix
+++ package.nix
@@ -1,7 +1,7 @@
 { ps-pkgs, ps-pkgs-ns }:
   with ps-pkgs;
   { repo = "https://github.com/ursi/purescript-elmish.git";
-    rev = "";
+    rev = "afbdab43f53b1259a880a234d74ac6945f7ad7b7";
     dependencies =
       let inherit (ps-pkgs-ns) ursi; in
       [ console
